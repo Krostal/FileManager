4d605fb4f5381a219c3291af2a4cac0f8555badf
Krostal <krostal1991@gmail.com>
2023-09-20 13:15:25 +0700

    removed @IBDesignable to fix bug in main.storyboard

--- FileManager/AuthorizationViewController/AuthorizationTextField.swift
+++ FileManager/AuthorizationViewController/AuthorizationTextField.swift
@@ -1,20 +1,20 @@
 import UIKit
 
-@IBDesignable
+//@IBDesignable
 class AuthorizationTextField: UITextField {
-    
+
     @IBInspectable var borderColor: UIColor = .systemRed {
         didSet {
             self.layer.borderColor = borderColor.cgColor
         }
     }
-    
+
     @IBInspectable var cornerRadius: CGFloat = 16 {
         didSet {
             self.layer.cornerRadius = cornerRadius
         }
     }
-        
+
     @IBInspectable var borderWidth: CGFloat = 1 {
         didSet {
             self.layer.borderWidth = borderWidth
